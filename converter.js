e69d79e84ddcfc512f5adde6c2c70203cb6a03e0
Anwesh Panda <pandaanwesh33@gmail.com>
2019-01-08 23:30:09 +0530

    URL feature
    URL is provided as command line argument.

--- converter.js
+++ converter.js
@@ -1,12 +1,16 @@
 var ffmpeg = require('fluent-ffmpeg');
-
+const fs = require('fs');
+const ytdl = require('ytdl-core');
 /**
  *    input - string, path of input file
  *    output - string, path of output file
  *    callback - function, node-style callback fn (error, result)        
  */
-var start=8;
-var duration=3.07;
+var start=2000;
+var duration=4000;
+var URL=process.argv[2];
+var ytVideo=ytdl(URL,{ filter: 'audioonly'})
+            .pipe(fs.createWriteStream('./ytAudio/music.mp3')); 
 
 function convert(input,output,start,duration,callback) {
     ffmpeg(input)
@@ -30,4 +34,4 @@ convert('sample_input/sample.mp4', './sample_output/output.mp3',start, duration,
    }
 });
 
-
+console.log(process.argv);
